4f2b48c4604849c2668baff06a5c3d543bd32c0a
zhisu <z2552wan@uwaterloo.ca>
2024-12-15 23:03:45 -0500

    unhide cursor in dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -60,17 +60,6 @@ return {
 		scratch = {
 			enabled = true,
 		},
-		indent = {
-			enabled = false,
-            scope = {
-                animate = {
-                    enabled = false,
-                    duration = {
-                        total = 100,
-                    }
-                },
-            }
-		},
 	},
 	keys = {
 		{
@@ -195,24 +184,25 @@ return {
 			end,
 		})
 		vim.g.toggle = require("snacks.toggle")
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "SnacksDashboardOpened",
-			callback = function()
-				local hl = vim.api.nvim_get_hl(0, { name = "Cursor", create = true })
-				hl.blend = 100
-				vim.api.nvim_set_hl(0, "Cursor", hl)
-				vim.cmd("set guicursor+=a:Cursor/lCursor")
-			end,
-		})
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "SnacksDashboardClosed",
-			callback = function()
-				local hl = vim.api.nvim_get_hl(0, { name = "Cursor", create = true })
-				hl.blend = 0
-				vim.api.nvim_set_hl(0, "Cursor", hl)
-				-- vim.opt.guicursor.append("a:Cursor/lCursor")
-				vim.cmd("set guicursor+=a:Cursor/lCursor")
-			end,
-		})
+        -- Hide cursor in dashboard
+		-- vim.api.nvim_create_autocmd("User", {
+		-- 	pattern = "SnacksDashboardOpened",
+		-- 	callback = function()
+		-- 		local hl = vim.api.nvim_get_hl(0, { name = "Cursor", create = true })
+		-- 		hl.blend = 100
+		-- 		vim.api.nvim_set_hl(0, "Cursor", hl)
+		-- 		vim.cmd("set guicursor+=a:Cursor/lCursor")
+		-- 	end,
+		-- })
+		-- vim.api.nvim_create_autocmd("User", {
+		-- 	pattern = "SnacksDashboardClosed",
+		-- 	callback = function()
+		-- 		local hl = vim.api.nvim_get_hl(0, { name = "Cursor", create = true })
+		-- 		hl.blend = 0
+		-- 		vim.api.nvim_set_hl(0, "Cursor", hl)
+		-- 		-- vim.opt.guicursor.append("a:Cursor/lCursor")
+		-- 		vim.cmd("set guicursor+=a:Cursor/lCursor")
+		-- 	end,
+		-- })
 	end,
 }
